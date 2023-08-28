b2201861902d241742a9a16632cdbdd2d6ba1636
l2k <ekyi0421@gmail.com>
2023-08-28 23:25:33 +0900

    build : buildSrc 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,12 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
+        plugins {
+            id(Plugins.ANDROID_APPLICATION)
+            id(Plugins.KOTLIN_ANDROID)
+            id(Plugins.HILT_ANDROID)
+            id(Plugins.KAPT)
+            id("com.google.relay") version "0.3.02"
+        }
 
 android {
     namespace = "com.nyang.weathercaminsights"
@@ -9,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.nyang.weathercaminsights"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -30,40 +35,71 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
+        dataBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = Versions.COMPOSE_UI
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    packagingOptions {
+        packagingOptions {
+            resources.excludes.add("META-INF/gradle/incremental.annotation.processors")
         }
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.lifecycleRuntimeKtx)
+    implementation(Dependencies.lifecycleRuntimeCompose)
+    implementation(Dependencies.activityCompose)
+    implementation(Dependencies.composeUI)
+    implementation(Dependencies.composeUIToolingPreview)
+    implementation("androidx.compose.ui:ui-tooling:1.1.1")
+    implementation(Dependencies.material)
+    implementation(Dependencies.composeMaterialWindowSize)
+    implementation(Dependencies.composeNavigation)
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.media3:media3-exoplayer:1.1.0")
+    testImplementation(Testing.composeUiTooling)
+    testImplementation(Testing.composeUiTestJunit)
+    testImplementation(Testing.composeUiTestManifest)
+    testImplementation(Testing.junit)
+    testImplementation(Testing.testExtJunit)
+    testImplementation(Testing.espressoCore)
+    implementation(Dependencies.gson)
+    implementation(Dependencies.hiltAndroid)
+    kapt(Dependencies.hiltCompiler)
+    implementation(Dependencies.hiltNavigationCompose)
+    implementation("androidx.compose.material:material:1.3.1")
+    implementation("androidx.compose.animation:animation:1.3.3")
+    implementation("androidx.compose.material:material-icons-extended:1.3.1")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+
+
+    // CameraX
+    implementation(Dependencies.cameraLifecycle)
+    implementation(Dependencies.cameraVideo)
+    implementation(Dependencies.cameraView)
+    implementation(Dependencies.cameraExtensions)
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-permissions:0.23.1")
+    implementation("com.google.android.exoplayer:exoplayer:2.18.1")
+
+    //implementation(project(mapOf("path" to ":rsp-module:data")))
+    // jetpack compose drawblepainter
+    implementation(Dependencies.accompanist_drawablepainter)
+    implementation(Dependencies.splashScreen)
+    implementation("com.google.accompanist:accompanist-drawablepainter:0.28.0")
+    implementation ("androidx.core:core-splashscreen:1.0.0-alpha01")
+    implementation ("androidx.compose.material:material-icons-extended:1.2.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id(Plugins.ANDROID_APPLICATION) version Versions.AGP apply false
+    id(Plugins.ANDROID_LIBRARY) version Versions.AGP apply false
+    id(Plugins.KOTLIN_ANDROID) version Versions.KOTLIN apply false
+    id(Plugins.HILT_ANDROID) version Versions.HILT_ANDROID apply false
+    id("org.jetbrains.kotlin.jvm") version Versions.KOTLIN apply false
+    id("org.jetbrains.dokka") version "1.8.20"
+}
+
+
+buildscript {
+    dependencies {
+        classpath(Dependencies.hiltPlugin)
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.7.10")
+    }
+}
+
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
