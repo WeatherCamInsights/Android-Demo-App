ef12cdcc697330540fbb23ddfaf07975b81bb85d
l2k <ekyi0421@gmail.com>
2023-09-11 20:30:40 +0900

    feat: cctv data model 추가

---
+++ app/src/main/java/com/nyang/weathercaminsights/domain/cctvData/model/CctvInput.kt
@@ -0,0 +1,25 @@
+package com.nyang.weathercaminsights.domain.cctvData.model
+
+/**
+ * cctv 요청 전문 data input
+ *
+ * @param apiKey 공개키
+ * @param type 도로 유형(ex: 고속도로 / its: 국도)
+ * @param cctvType CCTV 유형(1: 실시간 스트리밍(HLS) / 2: 동영상 파일 / 3: 정지 영상)
+ * @param minX 최소 경도 영역
+ * @param maxX 최대 경도 영역
+ * @param minY 최소 위도 영역
+ * @param maxY 최대 위도 영역
+ * @param getType 출력 결과 형식(xml, json / 기본: json)
+ *
+ */
+data class CctvInput(
+    val apiKey: String,
+    val type: String,
+    val cctvType: String,
+    val minX: Double,
+    val maxX: Double,
+    val minY: Double,
+    val maxY: Double,
+    val getType: String = "JSON"
+)
---
+++ app/src/main/java/com/nyang/weathercaminsights/domain/cctvData/model/CctvOutput.kt
@@ -0,0 +1,28 @@
+package com.nyang.weathercaminsights.domain.cctvData.model
+
+/**
+ * @param coordtype 좌표타입
+ * @param datacount CCTV 개수
+ * @param roadsectionid 도로 구간 ID
+ * @param filecreatetime 파일 생성 시간(YYYYMMDDHH24MISS)
+ * @param cctvtype CCTV 유형(1: 실시간 스트리밍(HLS) / 2: 동영상 파일 / 3: 정지 영상)
+ * @param cctvurl CCTV 영상 주소
+ * @param cctvresolution CCTV 해상도
+ * @param coordx 경도 좌표
+ * @param coordy 위도 좌표
+ * @param cctvformat CCTV 형식
+ * @param cctvname    CCTV 설치 장소명
+ */
+data class CctvOutput(
+    val coordtype: String,
+    val datacount: Int,
+    val roadsectionid: String,
+    val filecreatetime: String,
+    val cctvtype: String,
+    val cctvurl: String,
+    val cctvresolution: String,
+    val coordx: String,
+    val coordy: String,
+    val cctvformat: String,
+    val cctvname: String
+)
